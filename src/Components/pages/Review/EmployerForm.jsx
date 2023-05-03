9cd78c1d58a18dd0638eeca66385f756fc06745c
LucasPitzalis <lpitzalis@yahoo.fr>
2023-05-03 10:06:34 +0200

    EmployerForm finished

--- src/Components/pages/Review/EmployerForm.jsx
+++ src/Components/pages/Review/EmployerForm.jsx
@@ -14,11 +14,12 @@ export default function EmployerForm({throwMsg}) {
         {label: "La formation choisie semblait-elle répondre à son besoin ?", options: ["Oui, parfaitement", "Oui, partiellement", "Non", "Je ne sais pas"]},
         {label: "Qui était à l'initiative de cette formation ?", options: ["Vous-même", "Votre collaborateur", "Vous et votre collaborateur"]},
         {label: "Depuis la fin de la formation, votre collaborateur a-t-il pu mettre en pratique les connaissances acquises ?", options: ["Oui", "Oui, partiellement", "Non"]},
+        {label: "Depuis la fin de la formation, a quelle fréquence votre collaborateur a-t-il mis en pratique les connaissances acquises ?", options: ["Régulièrement", "Occasionnellement"]},
         {label: "A l'issue de la formation, avez-vous eu un entretien avec votre collaborateur pour faire le point sur l'apport de la formation ?", options: ["Oui", "Non"]}
     ]
 
     const reviewInitialState = { writerName: "", company: "", service: "", place: "", startDate: "", endDate: "", duration: "", 
-        mcq: [], needAnsweredComment: "", practiceFrequencyComment: "", learnerComment: "",
+        mcq: [], needAnsweredComment: "", practiceFrequencyComment: "", practiceSuggestions: "", learnerComment: "",
         rate: 3};
 
     mcq.forEach((question) => {
@@ -45,7 +46,7 @@ export default function EmployerForm({throwMsg}) {
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrorList([]);
-        axios.post('http://localhost:5000/api/review/submit', {review: review, type: "funding"})
+        axios.post('http://localhost:5000/api/review/submit', {review: review, type: "employer"})
             .then((res) => {
                 saveReview(prompt("Veuillez entrer le mot de passe fourni par Idoine Formation"))
             })
@@ -56,7 +57,7 @@ export default function EmployerForm({throwMsg}) {
 
     const saveReview = (password) => {
         setErrorList([]);
-        axios.post('http://localhost:5000/api/review/create', {review: review, type: "funding", password: password})
+        axios.post('http://localhost:5000/api/review/create', {review: review, type: "employer", password: password})
             .then((res) => {
                 setReview(reviewInitialState);
                 throwMsg(["Formulaire envoyé avec succès. Merci pour votre avis !"]);
@@ -95,12 +96,18 @@ export default function EmployerForm({throwMsg}) {
                             </div>
                             <TextInput {... {name: 'duration', label: 'Durée de la formation (en heures)', placeholder: 'Durée de la formation', handleChanges: handleChange, value: review.duration, isRequired: true}} />
                             <Mcq handleChanges={handleMcqChange} {...mcq[0]} />
-                            {/* {review.rates.map(({label, rate, comment}) => 
-                                <div key={label}>
-                                    <StarRating {...{rating: rate, setRating: handleRateChange, name: label, label: label}} />
-                                    <TextArea {...{name: label, label: 'Si non ou partiellement, pourquoi ?', placeholder: 'Vos remarques', handleChanges: handleCommentChange, value: comment, isRequired: false}}/>
-                                </div>
-                            )} */}
+                            <Mcq handleChanges={handleMcqChange} {...mcq[1]} />
+                            {["Oui, partiellement", "Non"].includes(review.mcq[1].answer) && 
+                                <TextArea {...{name: 'needAnsweredComment', label: `En quoi la formation ne répond-elle ${review.mcq[1].answer === "Non" ? "pas" : "pas complètement"} au besoin ?`, placeholder: 'Votre réponse...', handleChanges: handleChange, value: review.needAnsweredComment, isRequired: true}}/>}
+                            <Mcq handleChanges={handleMcqChange} {...mcq[2]} />
+                            <Mcq handleChanges={handleMcqChange} {...mcq[3]} />
+                            {["Oui", "Oui, partiellement"].includes(review.mcq[3].answer) && <Mcq handleChanges={handleMcqChange} {...mcq[4]} />}
+                            {["Oui, partiellement", "Non"].includes(review.mcq[3].answer) && 
+                                <TextArea {...{name: 'practiceFrequencyComment', label: `Pourquoi pensez-vous que votre collaborateur n'a pas été en mesure de mettre en pratique ${review.mcq[3].answer === "Non" ? "les" : "l'intégralité des"} connaissances acquises ?`, placeholder: 'Votre réponse...', handleChanges: handleChange, value: review.practiceFrequencyComment, isRequired: true}}/>}
+                            <TextArea {...{name: 'practiceSuggestions', label: 'Selon vous, qu\'est ce qui pourrait favoriser la mise en pratique de la formation ? (ex: Disposer de plus de temps de mise en pratique, être guidé par un collègue...) (facultatif)', placeholder: 'Vos suggestions...', handleChanges: handleChange, value: review.practiceSuggestions, isRequired: false}}/>
+                            <Mcq handleChanges={handleMcqChange} {...mcq[5]} />
+                            {review.mcq[5].answer === "Oui" && <TextArea {...{name: 'learnerComment', label: 'Quelles ont été les remarques et observations de votre collaborateur à l\'issue de cet entretien ? (facultatif)', placeholder: 'Remarques/observations du collaborateur...', handleChanges: handleChange, value: review.learnerComment, isRequired: false}}/>}
+                            <StarRating {...{rating: review.rate, setRating: handleChange, name: 'rate', label: 'Les objectifs visés par la formation ont-ils étés atteints selon vous ?'}} />
                             <button className="btn btn-primary" type="submit">Envoyer</button>
                         </div>
                     </form>
